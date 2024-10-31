70f49753474f7dace6c5ee802b2ec915f4d8d160
Juliano Costa Silva <apfjuliano@gmail.com>
2024-10-31 19:22:37 -0300

    Fixing About Page aligment

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -27,18 +27,20 @@ const About = () => {
           <p className={styles.rqe}>{`RQE - ${profile.rqe}`}</p>
         </div>
 
-        <div className={styles.description}>
-          <p><strong>Médica, graduada em Medicina pela Universidade Federal de Minas Gerais (UFMG), com residência médica em Radiologia e Diagnóstico por Imagem no Hospital das Clínicas, Mestrado e Doutorado pela UFMG.</strong></p>
-          <p><strong>Radiologista com ênfase nas áreas de Oncorradiologia e Medicina Interna</strong>, atuando nas áreas de Ressonância Magnética, Tomografia Computadorizada e PET/CT</p>
-          <p>Foi <strong>Foi Professora Adjunta do Departamento de Anatomia e Imagem da Faculdade de Medicina da UFMG</strong> e <strong>Membro de várias comissões do Colégio Brasileiro de Radiologia e Diagnóstico por Imagem (CBR)</strong>, onde atuou, na <strong>gestão 2021-2022</strong>, como <strong>Diretora Científica</strong>. No momento, atua como <strong>Diretora Científica da Sociedade de Radiologia de Minas Gerais (SRMG)</strong>.</p>
-        </div>
+        <div className={styles.content}>
+          <div className={styles.description}>
+            <p><strong>Médica, graduada em Medicina pela Universidade Federal de Minas Gerais (UFMG), com residência médica em Radiologia e Diagnóstico por Imagem no Hospital das Clínicas, Mestrado e Doutorado pela UFMG.</strong></p>
+            <p><strong>Radiologista com ênfase nas áreas de Oncorradiologia e Medicina Interna</strong>, atuando nas áreas de Ressonância Magnética, Tomografia Computadorizada e PET/CT</p>
+            <p>Foi <strong>Foi Professora Adjunta do Departamento de Anatomia e Imagem da Faculdade de Medicina da UFMG</strong> e <strong>Membro de várias comissões do Colégio Brasileiro de Radiologia e Diagnóstico por Imagem (CBR)</strong>, onde atuou, na <strong>gestão 2021-2022</strong>, como <strong>Diretora Científica</strong>. No momento, atua como <strong>Diretora Científica da Sociedade de Radiologia de Minas Gerais (SRMG)</strong>.</p>
+          </div>
 
-        <p className={styles.fellows}>Foi <strong>Fellow visitante</strong> de várias instituições, destacando-se:</p>
-        <ul className={styles.fellow}>
-          {profile.fellow.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
+          <p className={styles.fellows}>Foi <strong>Fellow visitante</strong> de várias instituições, destacando-se:</p>
+          <ul className={styles.fellow}>
+            {profile.fellow.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
