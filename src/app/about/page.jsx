7f397e3b256647731c8d53934f90eade910e4dc0
Juliano Costa Silva <apfjuliano@gmail.com>
2024-10-31 18:56:30 -0300

    Creating About Page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,46 @@
+import Image from 'next/image';
 import styles from './about.module.css'
+import photo from '@/assets/images/medico.png';
+import { profile } from "@/utils/constants";
 
 const About = () => {
   return (
-    <main className={styles.container}>
-      <div>Sobre page</div>
-    </main>
+    <section className={styles.container}>
+      <div className={styles.image}>
+        <Image
+          className={styles.photo}
+          src={photo}
+          alt={`${profile.prefix} ${profile.name}`}
+          width={365}
+          height={365}
+          // sizes="(max-width: 365px) 150px, (max-width: 1200px) 20vw, 365px"
+        />
+      </div>
+      <div className={styles.curriculum}>
+        <div className={styles.intro}>
+          <p className={styles.name}>{`${profile.prefix} ${profile.name}`}</p>
+          <p className={styles.degrees}>{profile.degrees.join(', ')}</p>
+        </div>
+        <div className={styles.docs}>
+          <p className={styles.specialty}>{profile.specialty}</p>
+          <p className={styles.crm}>{`CRM ${profile.crm}/${profile.crmState}`}</p>
+          <p className={styles.rqe}>{`RQE - ${profile.rqe}`}</p>
+        </div>
+
+        <div className={styles.description}>
+          <p><strong>Médica, graduada em Medicina pela Universidade Federal de Minas Gerais (UFMG), com residência médica em Radiologia e Diagnóstico por Imagem no Hospital das Clínicas, Mestrado e Doutorado pela UFMG.</strong></p>
+          <p><strong>Radiologista com ênfase nas áreas de Oncorradiologia e Medicina Interna</strong>, atuando nas áreas de Ressonância Magnética, Tomografia Computadorizada e PET/CT</p>
+          <p>Foi <strong>Foi Professora Adjunta do Departamento de Anatomia e Imagem da Faculdade de Medicina da UFMG</strong> e <strong>Membro de várias comissões do Colégio Brasileiro de Radiologia e Diagnóstico por Imagem (CBR)</strong>, onde atuou, na <strong>gestão 2021-2022</strong>, como <strong>Diretora Científica</strong>. No momento, atua como <strong>Diretora Científica da Sociedade de Radiologia de Minas Gerais (SRMG)</strong>.</p>
+        </div>
+
+        <p className={styles.fellows}>Foi <strong>Fellow visitante</strong> de várias instituições, destacando-se:</p>
+        <ul className={styles.fellow}>
+          {profile.fellow.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -20,3 +20,27 @@ export const menuItems = [
     url: '/about',
   },
 ];
+
+export const profile = {
+  prefix: 'Dra.',
+  name: 'Luciana Costa Silva',
+  degrees: ['MD', 'MSc', 'PhD'],
+  crm: '28151',
+  crmState: 'MG',
+  rqe: '19315',
+  specialty: 'Radiologista',
+  description: [
+    'Médica, graduada em Medicina pela Universidade Federal de Minas Gerais (UFMG), com residência médica em Radiologia e Diagnóstico por Imagem no Hospital das Clínicas, Mestrado e Doutorado pela UFMG.',
+    'É radiologista com ênfase nas áreas de Oncorradiologia e Medicina Interna, atuando nas áreas de Ressonância Magnética, Tomografia Computadorizada e PET/CT',
+    'Foi Professora Adjunta do Departamento de Anatomia e Imagem da Faculdade de Medicina da UFMG e Membro de várias comissões do Colégio Brasileiro de Radiologia e Diagnóstico por Imagem (CBR), onde atuou, na gestão 2021-2022, como Diretora Científica. No momento, atua como Diretora Científica da Sociedade de Radiologia de Minas Gerais (SRMG)',
+  ],
+  fellow: [
+    'Beth Israel Diaconess Medical Center, Harvard Medical School, Estados Unidos',
+    'Royal Marsden Hospital, Inglaterra',
+    'Université Libre de Bruxelles, Bélgica',
+  ],
+  contact: {
+    phone: '+55(31)99999-9999',
+    email: 'lucianacosta@me.com',
+  },
+};
