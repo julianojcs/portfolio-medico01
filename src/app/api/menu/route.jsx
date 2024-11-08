cd3884f59f73e64ddd281dd1de82ec1a3ac8064e
Juliano Costa Silva <apfjuliano@gmail.com>
2024-11-08 18:10:19 -0300

    Updating Cors Allow All

--- src/app/api/menu/route.jsx
+++ src/app/api/menu/route.jsx
@@ -6,7 +6,12 @@ export const GET = async (req, res) => {
   try {
     // await connectDB();
 
-    return new Response(JSON.stringify(menuItems), { status: 200 });
+    return new Response(JSON.stringify(menuItems), {
+      status: 200,
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    });
   } catch (error) {
     console.log(error);
     return new Response(error, { status: 500 });
