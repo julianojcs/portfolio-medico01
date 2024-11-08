d94d9778efc2e062274d522a06d101cbf3c1fd45
Juliano Costa Silva <apfjuliano@gmail.com>
2024-11-08 18:41:08 -0300

    Update: fetching data from json file

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,42 +1,24 @@
 import Link from 'next/link';
-// import { menuItems } from "@/utils/constants";
-// ou
-// import menuData from '@/utils/db.json';
+// import { getMenuItems } from '@/db/menu';
 import styles from './menu.module.css'
+import menuData from '@/utils/db.json';
 
-const fetchMenuItems = async () => {
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/menu`);
-    // const response = await fetch('/api/menu'); // Como o fetch é feito no cliente, a URL deve ser absoluta
-    if (!response.ok) {
-      throw new Error('Failed to fetch menu items');
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-};
+// const menuItems = getMenuItems();
 
-export const Menu = async ({pathname}) => {
-  const menuItems = await fetchMenuItems();
-  // const {menuItems } = menuData;
-
-  if (menuItems) {
-    return (
-      <>
-        {menuItems != null &&
-          menuItems.map((item) => (
-            <Link
-              key={item.title}
-              href={item.url}
-              className={`${styles.item} ${pathname === item.url && styles.active}`}
-            >
-              {item.title}
-            </Link>
-          ))}
-      </>
-    );
-  }
+export const Menu = ({pathname}) => {
+  const { menuItems } = menuData;
+  return (
+    <>
+      {menuItems != null &&
+        menuItems.map((item) => (
+          <Link
+            key={item.title}
+            href={item.url}
+            className={`${styles.item} ${pathname === item.url && styles.active}`}
+          >
+            {item.title}
+          </Link>
+        ))}
+    </>
+  );
 };
